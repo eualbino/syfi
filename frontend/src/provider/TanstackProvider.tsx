6f7e49c100aa527eb49ce9c65eeeed6b67a22207
eualbino <lucas.g.apostolo@gmail.com>
2024-02-29 14:10:33 -0300

    Feat: Delete Button
    Foi feito o botao de deletar os dados

--- frontend/src/provider/TanstackProvider.tsx
+++ frontend/src/provider/TanstackProvider.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const TanstackProvider = ({ children }: { children: ReactNode }) => {
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			{children}
-			<ReactQueryDevtools initialIsOpen={false}/>	
 		</QueryClientProvider>
 	);
 };
